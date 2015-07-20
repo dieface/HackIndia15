91a73ace5e772be0f13f8842c7c961a699c3ee1a
Ritvik Choudhary <ritvik@outlook.com>
2015-07-20 16:21:08 +0530

    HackIndia
    HackIndia

---
+++ jupiter.ios/jp.framework/Headers/jp.h
@@ -0,0 +1,17 @@
+/* Jupiter - Optimizing JavaScript Compiler/Runtime
+ * Copyright (C) */
+
+#ifndef CYCRIPT_CYCRIPT_H
+#define CYCRIPT_CYCRIPT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void CYListenServer(short port);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif/*CYCRIPT_CYCRIPT_H*/
